444f996ace75d2ec7db39ceac96cbef01f353846
Garrett Peake <gepeake@gmail.com>
2020-06-27 17:29:40 -0700

    Change timeout control

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fade(t) {
     let elem = document.getElementById("blanker")
     for(alpha = 100; alpha > 0; alpha--) {
         elem.setAttribute("style", "opacity: " + (alpha/100.0).toString() + ";");
-        await new Promise(r => setTimeout(r, t/100.0*1000.0));
+        setTimeout(true, t/100.0*1000.0);
     }
     elem.parentNode.removeChild(elem);
   }
