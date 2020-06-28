5e1f40d8bbe772c26dd9a4d003150cdf8d23071c
Garrett Peake <gepeake@gmail.com>
2020-06-28 01:34:07 -0700

    Add Temp Graphics and Update Animations

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+var alpha = 1;
+var counter = 0;
+var resolution = 100.0;
 function fade(t) {
-    let elem = document.getElementById("blanker")
-    for(alpha = 100; alpha > 0; alpha--) {
-        elem.setAttribute("style", "opacity: " + (alpha/100.0).toString() + ";");
-        setTimeout(true, t/100.0*1000.0);
-    }
+  let elem = document.getElementById("blanker");
+  counter++;
+  setTimeout(() => {
+    console.log(alpha);
+    alpha -= 1.001 / resolution;
+    if (counter < resolution) {
+      elem.setAttribute("style", "opacity: ".concat(alpha.toString(), ";"));
+      fade(t);
+    } else
+      setTimeout(() => {
+        elem.parentNode.removeChild(elem);
+      }, 2 * t);
+  }, t / resolution * 1000);
+}
+
+function trackIntro() {
+  document.getElementById('cardintro').addEventListener('ended', function (e) {
+    let elem = document.getElementById('cardintro');
     elem.parentNode.removeChild(elem);
-  }
+
+  }, false);
+}
