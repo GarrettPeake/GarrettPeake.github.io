c3f565f6d5eb062d131acffec48a17e00b47a641
Garrett Peake <gepeake@gmail.com>
2020-08-05 17:23:53 -0700

    Working fade

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 function fade(elem, t) {
   // Fades in or out an element in time t, to fade out make t negative
-  elem.setAttribute("style", "z-index: 9999;");
-  var alpha = t>0?1:0;
-  var counter = 0;
-  var resolution = 100.0;
-  function fadeInner(elem, t) {
-    console.log("run");
-    counter++;
-    setTimeout(() => {
-      console.log(alpha);
-      alpha += t>0 ? -1.001 / resolution : 1.001 / resolution;
-      if (counter < resolution) {
-        elem.setAttribute("style", "opacity: ".concat(alpha.toString(), ";"));
-        fadeInner(elem, t);
-      } else
-        setTimeout(() => {
-          elem.setAttribute("style", "opacity: 0;");
-          elem.setAttribute("style", "z-index: -9999;");
-        }, Math.abs(t));
-    }, Math.abs(t) / resolution * 1000);
-  }
-  fadeInner(elem, t);
+  // Decides whether to fade in or out
+  let resolution = 100.0;
+  setTimeout(() => {
+    let curr = window.getComputedStyle(elem).getPropertyValue("opacity");
+    elem.style.opacity = parseFloat(curr) + (t > 0 ? -1.001 / resolution : 1.001 / resolution);
+    if (curr > 0)
+      fade(elem, t);
+    else if (t > 0)
+      elem.style.zIndex = -999
+  }, Math.abs(t) / resolution * 1000);
 }
 
 function fadeBlanker() {
+  let t = .5;
   let elem = document.getElementById("blanker");
-  document.addEventListener('DOMContentLoaded', (event) => {
-    //event.preventDefault();
-    //event.returnValue = '';
-    elem.setAttribute("style", "z-index: 9999;");
-    fade(elem, -1)
-  });
-  fade(elem, 1);
+  fade(elem, t);
+  window.onbeforeunload = function(){
+    elem.style.zIndex = 99;
+    fade(elem, -t)
+  };
+}
+
+function moveEverything(){
+  return 1;
 }
 
 function trackIntro() {
-  document.getElementById('cardintro').addEventListener('ended', function (e) {
-    let elem = document.getElementById('cardintro');
-    fade(elem, 2);
-  }, false);
+  let t = .5;
+  let elem = document.getElementById('cardintro');
+  let blanker2 = document.getElementById("blanker2");
+  blanker2.style.zIndex = 97;
+  // Testing
+  fade(elem, t);
+  fade(blanker2, t)
+  // document.getElementById('cardintro').addEventListener('ended', function (e) {
+  //   let elem = document.getElementById('cardintro');
+  //   fade(elem, t);
+  //   fade(blanker2, t)
+  //   setTimeout(() => {
+  //     moveEverything();
+  //   }, t);
+  // }, false);
 }
