1a01e47594328c898e754b529fd9df89e9c2b08d
Garrett Peake <gepeake@gmail.com>
2020-08-19 00:01:52 -0700

    New Colors, Card animations in CSS rather than video

---
+++ plans.md
@@ -0,0 +1,9 @@
+Card pops straight up
+Card bounces shallow
+Card flips top backward
+Card zooms to fill frame
+Cards snake crawl to fill row
+	-box shadow slowly appears
+    -buttons appear
+	-logo and card background fade
+Bouncing scrool indicator appears, middle pill
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function fade(elem, t) {
-  let resolution = 100.0;
+  let resolution = 200.0;
   setTimeout(() => {
     let curr = window.getComputedStyle(elem).getPropertyValue("opacity");
     elem.style.opacity = parseFloat(curr) + (t > 0 ? -1.001 / resolution : 1.001 / resolution);
@@ -11,7 +11,7 @@ function fade(elem, t) {
 }
 
 function fadeBlanker() {
-  let t = .5;
+  let t = 1;
   let elem = document.getElementById("blanker");
   fade(elem, t);
   window.onbeforeunload = function(){
@@ -20,24 +20,6 @@ function fadeBlanker() {
   };
 }
 
-function moveEverything(){
-  return 1;
-}
-
-function trackIntro() {
-  let t = .5;
-  let elem = document.getElementById('cardintro');
-  let blanker2 = document.getElementById("blanker2");
-  blanker2.style.zIndex = 97;
-  // Testing
-  fade(elem, t);
-  fade(blanker2, t)
-  // document.getElementById('cardintro').addEventListener('ended', function (e) {
-  //   let elem = document.getElementById('cardintro');
-  //   fade(elem, t);
-  //   fade(blanker2, t)
-  //   setTimeout(() => {
-  //     moveEverything();
-  //   }, t);
-  // }, false);
+function animateIntro() {
+  return
 }
